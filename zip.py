cc21f212281df9d21d9e76230958d5fae32807fe
Ilgın <ilgin.guven0@gmail.com>
2025-06-30 16:08:03 +0300

    Create zip.py

---
+++ zip.py
@@ -0,0 +1,9 @@
+
+students = [ "j" , "m" , "v", "m"]
+departments = [ "math" , "statistics ","physics","astrology"]
+ages = [23,30,26,22]
+
+list(zip(students,departments,ages) 
+     # ayrı listeleri tek bir liste içerisine,  her birinde bulunan elemanları oldukları sırayla alarak yeni bir eleman gibi oluşturur. 
+     # j math 23 (ilk eleman) 
+     
