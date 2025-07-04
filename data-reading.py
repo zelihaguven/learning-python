019be7a4e4b972a4f2754509efc25d33f60e52ee
Ilgın <ilgin.guven0@gmail.com>
2025-07-04 19:26:32 +0300

    Create data-reading.py

---
+++ data-reading.py
@@ -0,0 +1,15 @@
+# veri okuma : 
+
+df = pd.read_csv("dataset.csv") 
+
+# veri okuma yapmayı sağlar 
+
+# read_table 
+# read_excel 
+
+ds = sns.load_dataset("example") 
+
+df.shape
+df.info()
+df.head()
+df.columns # nameler 
